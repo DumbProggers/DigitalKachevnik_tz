331b5d816d1f476314c17ad8c8253726a00d5f0c
Vlad <vladislavbymain@mail.ru>
2024-03-08 13:26:43 +0900

    Initialized project
    Создан пустой проект с базовыми необходимыми библиотеками

---
+++ DigitalKachevnik/src/main/java/dumbProggers/DigitalKachevnik/DigitalKachevnikApplication.java
@@ -0,0 +1,13 @@
+package dumbProggers.DigitalKachevnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalKachevnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalKachevnikApplication.class, args);
+	}
+
+}
---
+++ DigitalKachevnik/src/test/java/dumbProggers/DigitalKachevnik/DigitalKachevnikApplicationTests.java
@@ -0,0 +1,13 @@
+package dumbProggers.DigitalKachevnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalKachevnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
